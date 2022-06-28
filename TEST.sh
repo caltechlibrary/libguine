a57649ee8e895ead67697516c078a0975a7c9b04
Tommy Keswick <tkeswick@caltech.edu>
2022-06-28 16:59:04 -0700

    loop over changed files

--- TEST.sh
+++ TEST.sh
@@ -3,4 +3,8 @@
 if [ "$#" -eq 0 ]; then
     echo "Usage: /bin/sh $0 FILE" >&2
     exit 0
+elif [ "${FILE##*.}" = "html" ] || [ "${FILE##*.}" = "scss" ] || [ "${FILE##*.}" = "shtm" ]; then
+    echo "TODO"
+else
+    echo "Skipping ${FILE}"
 fi
