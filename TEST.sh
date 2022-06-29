0e7cdcbe526d420960be6c31e1ee8253b09f51d3
Tommy Keswick <tkeswick@caltech.edu>
2022-06-28 17:22:57 -0700

    use correct variable

--- TEST.sh
+++ TEST.sh
@@ -3,8 +3,8 @@
 if [ "$#" -eq 0 ]; then
     echo "Usage: /bin/sh $0 FILE" >&2
     exit 0
-elif [ "${FILE##*.}" = "html" ] || [ "${FILE##*.}" = "scss" ] || [ "${FILE##*.}" = "shtm" ]; then
+elif [ "${1##*.}" = "html" ] || [ "${1##*.}" = "scss" ] || [ "${1##*.}" = "shtm" ]; then
     echo "TODO"
 else
-    echo "Skipping ${FILE}"
+    echo "Skipping ${1}"
 fi
