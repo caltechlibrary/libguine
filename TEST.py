fdee8dafd8dceb8f03bea13314823a9fb907565e
Tommy Keswick <tkeswick@caltech.edu>
2022-06-29 18:05:10 -0700

    filter files upfront; rearrange playwright; handle templates

--- TEST.py
+++ TEST.py
@@ -9,63 +9,70 @@ def main(
     admin_base_url: "base url for admin access",  # type: ignore
     admin_username: "username for admin access",  # type: ignore
     admin_password: "password for admin access",  # type: ignore
+    github: ("optional github context object", "option", "g"),  # type: ignore
 ):
     os.makedirs("artifacts", exist_ok=True)
-    # html and shtm files may need includes processed
-    if file.endswith(".html") or file.endswith(".shtm"):
-        pass
-        if file.split("-")[0] == "head":
-            pass
-        elif file.split("-")[0] == "template":
-            pass
-        elif file.split("-")[0] == "footer":
-            pass
-        elif file.split("-")[0] == "header":
-            pass
-        # TODO compile HTML
-    # scss files may need compiling
-    elif file.endswith(".scss"):
-        compiled_css = "artifacts/TEST.css"
-        result = subprocess.run(
-            [
-                "sass",
-                "--no-charset",
-                "--no-source-map",
-                "TEST.scss",
-                compiled_css,
-            ]
-        )
-        print(result)
-        try:
-            with sync_playwright() as playwright:
-                b = playwright.firefox.launch()
-                p = b.new_page(base_url=admin_base_url, record_video_dir="artifacts")
-                p.goto("/libguides")
-                p.fill("#s-libapps-email", admin_username)
-                p.fill("#s-libapps-password", admin_password)
-                p.screenshot(full_page=True, path="artifacts/1.png")
-                p.click("#s-libapps-login-button")
-                p.screenshot(full_page=True, path="artifacts/2.png")
-                # p.wait_for_load_state()
-                # p.goto("/libguides/lookfeel.php?action=1")
-                p.click("#s-lib-app-anchor")
-                p.screenshot(full_page=True, path="artifacts/3.png")
-                p.click("#s-lib-app-menu a:text('LibGuides')")
-                p.screenshot(full_page=True, path="artifacts/4.png")
+    try:
+        with sync_playwright() as playwright:
+            b = playwright.firefox.launch()
+            p = b.new_page(base_url=admin_base_url, record_video_dir="artifacts")
+            p.goto("/libapps/login.php")
+            p.fill("#s-libapps-email", admin_username)
+            p.fill("#s-libapps-password", admin_password)
+            p.click("#s-libapps-login-button")
+            p.click("#s-lib-app-anchor")
+            p.click("#s-lib-app-menu a:text('LibGuides')")
+            # html and shtm files may need includes processed
+            if file.endswith(".html") or file.endswith(".shtm"):
+                if file.split("-")[0] == "head":
+                    pass
+                elif file.split("-")[0] == "template":
+                    template_code = "<!-- WARNING: GENERATED CODE *CHANGES WILL BE OVERWRITTEN* -->\n"
+                    if github:
+                        template_code += f"<!-- see github.com/{github.repository_owner}/{github.repository}/commit/{github.sha} -->"
+                    with open(file, "r") as f:
+                        template_code += f.read()
+                    p.click("#s-lg-admin-command-bar a:text('Admin')")
+                    p.click("#s-lg-admin-command-bar a:text('Look & Feel')")
+                    p.click("#s-lib-admin-tabs a:text('Page Layout')")
+                    if file.split("-")[1] == "guide":
+                        p.click("#s-lib-admin-tabs a:text('Guide')")
+                        p.click("#s-lg-guide-templates_link")
+                        p.fill("#s2id_autogen2_search", file.split(".")[0])
+                        p.fill("#template_code", template_code)
+                        p.click("#btn-save-template")
+                    if file.split("-")[1] == "search":
+                        p.click("#s-lib-admin-tabs a:text('Search')")
+                        p.click("#s-lg-tpl_link")
+                        p.fill("#s2id_autogen3", file.split(".")[0])
+                        p.fill("#template_code", template_code)
+                        p.click("#btn-save-template")
+                elif file.split("-")[0] == "footer":
+                    pass
+                elif file.split("-")[0] == "header":
+                    pass
+                # TODO compile HTML
+            # scss files may need compiling
+            elif file.endswith(".scss"):
+                compiled_css = "artifacts/TEST.css"
+                subprocess.run(
+                    [
+                        "sass",
+                        "--no-charset",
+                        "--no-source-map",
+                        "TEST.scss",
+                        compiled_css,
+                    ]
+                )
                 p.click("#s-lg-admin-command-bar a:text('Admin')")
-                p.screenshot(full_page=True, path="artifacts/5.png")
                 p.click("#s-lg-admin-command-bar a:text('Look & Feel')")
-                p.screenshot(full_page=True, path="artifacts/6.png")
                 p.click("#s-lib-admin-tabs a:text('Custom JS/CSS')")
-                p.screenshot(full_page=True, path="artifacts/7.png")
                 p.click("#s-lg-include-files_link")
-                p.screenshot(full_page=True, path="artifacts/8.png")
                 p.set_input_files("#include_file", compiled_css)
-                p.screenshot(full_page=True, path="artifacts/9.png")
-                b.close()
-        except PlaywrightTimeoutError as e:
-            print(str(e))
-            return
+            b.close()
+    except PlaywrightTimeoutError as e:
+        print(str(e))
+        return
 
 
 if __name__ == "__main__":
