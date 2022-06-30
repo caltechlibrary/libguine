4d4a657155a2ad20b1ff731caab3a88363bc7e91
Tommy Keswick <tkeswick@caltech.edu>
2022-06-29 22:31:47 -0700

    screenshot seems to make it work, but at which point?

--- TEST.py
+++ TEST.py
@@ -44,6 +44,7 @@ def main(
                         p.fill("#s2id_autogen2_search", file.split(".")[0])
                         p.press("#s2id_autogen2_search", "Enter")
                         p.wait_for_selector("#template_code")
+                        p.screenshot(full_page=True, path="artifacts/1.png")
                         p.fill("#template_code", template_code)
                         p.click("#btn-save-template")
                         p.wait_for_load_state()
