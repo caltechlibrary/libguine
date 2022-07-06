d2d1e136c1f6d1a8ffc76c69a382b05dbfb96e4d
Tommy Keswick <tkeswick@caltech.edu>
2022-07-05 17:08:16 -0700

    separate compile and deploy scripts

---
+++ TEST-compile.py
@@ -0,0 +1,79 @@
+import json
+import os
+import subprocess
+
+
+def main(
+    file: "modified file from which to build artifacts",  # type: ignore
+    groups: '{"groups":[{"slug":"foo","id":"999"},{…}]}',  # type: ignore
+    github_commit: ("optional github commit path", "option", "g"),  # type: ignore
+):
+    # TODO optimize
+    if file.endswith(".scss"):
+        # avoid redundant artifact creation
+        if os.path.isfile(f"artifacts/custom.css"):
+            return
+        # NOTE requires `sass` command
+        subprocess.run(
+            [
+                "sass",
+                "--no-charset",
+                "--no-source-map",
+                "TEST.scss",  # TODO
+                "artifacts/TEST.css",  # TODO
+            ]
+        )
+    elif file.endswith(".html") or file.endswith(".shtm"):
+        target = file.split("-")[0]
+        slugs = [g["slug"] for g in json.loads(groups)["groups"]]
+        scopes = slugs.append("system")
+        scope = (
+            file.split(".")[0].split("-")[-1]
+            if file.split(".")[0].split("-")[-1] in scopes
+            else None
+        )
+        # avoid redundant artifact creation
+        if os.path.isfile(f"artifacts/{target}--{scope}.html"):
+            return
+        html = "<!-- WARNING: GENERATED CODE *EDITS WILL BE OVERWRITTEN* -->\n"
+        if github_commit:
+            html += (
+                f"<!-- see github.com/{github_commit[:len(github_commit) - 33]} -->\n\n"
+            )
+        if target == "template" or target == "head":
+            with open(file) as f:
+                html += f.read()
+            with open(f'artifacts/{file.split("/")[-1]}', "w") as f:
+                f.write(html)
+        elif target == "header" or target == "footer":
+            fileobject = open(f"{target}-wrapper.shtm")
+            html += parse_nested_includes(fileobject, html, scope)
+            fileobject.close()
+            with open(f'artifacts/{file.split("/")[-1]}', "w") as f:
+                f.write(html)
+
+    def parse_nested_includes(fileobject, html, scope=None):
+        if scope is None:
+            for s in scopes:
+                html += parse_nested_includes(fileobject, html, s)
+        for line in fileobject:
+            if line.strip().startswith("<!--#include"):
+                included_file = line.split("'")[1]
+                if included_file.split(".")[0].endswith("-GROUP"):
+                    # open the GROUP/scope file and read its lines
+                    fo = open(included_file.replace("GROUP", scope))
+                    html += parse_nested_includes(fo, html, scope)
+                    fo.close()
+                else:
+                    # open the file and read its lines
+                    fo = open(included_file)
+                    html += parse_nested_includes(fo, html, scope)
+                    fo.close()
+            else:
+                html += line
+        return html
+
+
+if __name__ == "__main__":
+    # fmt: off
+    import plac; plac.call(main)
---
+++ TEST-deploy.py
@@ -0,0 +1,141 @@
+import json
+import os
+
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+
+
+def main(
+    admin_base_url: "base url for admin access",  # type: ignore
+    admin_username: "username for admin access",  # type: ignore
+    admin_password: "password for admin access",  # type: ignore
+    groups: '{"groups":[{"slug":"foo","id":"999"},{…}]}',  # type: ignore
+):
+    # TODO optimize
+    for item in os.scandir("artifacts"):
+        try:
+            with sync_playwright() as playwright:
+                b = playwright.firefox.launch()
+                p = b.new_page(base_url=admin_base_url, record_video_dir="artifacts")
+                p.goto("/libapps/login.php")
+                p.fill("#s-libapps-email", admin_username)
+                p.fill("#s-libapps-password", admin_password)
+                p.click("#s-libapps-login-button")
+                p.click("#s-lib-app-anchor")
+                p.click("#s-lib-app-menu a:text('LibGuides')")
+                if item.endswith(".css"):
+                    with open(item) as f:
+                        css = f.read()
+                    p.click("#s-lg-admin-command-bar a:text('Admin')")
+                    p.click("#s-lg-admin-command-bar a:text('Look & Feel')")
+                    p.click("#s-lib-admin-tabs a:text('Custom JS/CSS')")
+                    p.click("#s-lg-include-files_link")
+                    p.set_input_files("#include_file", css)
+                elif item.endswith(".html"):
+                    target = item.split("-")[0]
+                    slugs = [g["slug"] for g in json.loads(groups)["groups"]]
+                    scopes = slugs.append("system")
+                    scope = (
+                        item.split(".")[0].split("-")[-1]
+                        if item.split(".")[0].split("-")[-1] in scopes
+                        else None
+                    )
+                    with open(item) as f:
+                        html = f.read()
+                    if target == "template":
+                        p.click("#s-lg-admin-command-bar a:text('Admin')")
+                        p.click("#s-lg-admin-command-bar a:text('Look & Feel')")
+                        p.click("#s-lib-admin-tabs a:text('Page Layout')")
+                        if item.split("-")[1] == "guide":
+                            p.click("#s-lib-admin-tabs a:text('Guide')")
+                            p.click("#s-lg-guide-templates_link")
+                            p.click("#select2-chosen-2")
+                            # NOTE template must already exist
+                            # TODO account for template not found condition
+                            p.fill("#s2id_autogen2_search", item.split(".")[0])
+                            p.press("#s2id_autogen2_search", "Enter")
+                            # NOTE template takes time to load after select
+                            p.wait_for_load_state("networkidle")
+                            p.fill("#template_code", html)
+                            p.click("#btn-save-template")
+                            # NOTE must wait for success before moving on
+                            p.wait_for_selector("#btn-save-template.btn-success")
+                        if item.split("-")[1] == "search":
+                            p.click("#s-lib-admin-tabs a:text('Search')")
+                            p.click("#s-lg-tpl_link")
+                            p.click("#select2-chosen-3")
+                            # NOTE template must already exist
+                            # TODO account for template not found condition
+                            p.fill("#s2id_autogen3_search", item.split(".")[0])
+                            p.press("#s2id_autogen3_search", "Enter")
+                            # NOTE template takes time to load after select
+                            p.wait_for_load_state("networkidle")
+                            p.fill("#template_code", html)
+                            p.click("#btn-save-template")
+                            # NOTE must wait for success before moving on
+                            p.wait_for_selector("#btn-save-template.btn-success")
+                    elif target == "head":
+                        if scope == "system":
+                            p.goto("/libguides/lookfeel.php?action=1")
+                            p.fill("#jscss_code", html)
+                            p.click("#s-lg-btn-save-jscss")
+                            # NOTE must wait for success before moving on
+                            p.wait_for_selector("#s-lg-btn-save-jscss.btn-success")
+                        else:
+                            for group in json.loads(groups)["groups"]:
+                                if scope == group["slug"]:
+                                    p.goto(
+                                        f'/libguides/groups.php?action=3&group_id={group["id"]}'
+                                    )
+                                    p.fill("#jscss_code", html)
+                                    p.click("#s-lg-btn-save-jscss")
+                                    # NOTE must wait for success before moving on
+                                    p.wait_for_selector(
+                                        "#s-lg-btn-save-jscss.btn-success"
+                                    )
+                    elif target == "header":
+                        if scope == "system":
+                            p.goto("/libguides/lookfeel.php?action=0")
+                            p.fill("#banner_html", html)
+                            p.click("#banner_html + .btn-primary")
+                            # NOTE must wait for success before moving on
+                            p.wait_for_selector("#banner_html + .btn-success")
+                        else:
+                            for group in json.loads(groups)["groups"]:
+                                if scope == group["slug"]:
+                                    p.goto(
+                                        f'/libguides/groups.php?action=2&group_id={group["id"]}'
+                                    )
+                                    p.fill("#banner_html", html)
+                                    p.click("#banner_html + .btn-primary")
+                                    # NOTE must wait for success before moving on
+                                    p.wait_for_selector("#banner_html + .btn-success")
+                    elif target == "footer":
+                        if scope == "system":
+                            p.goto("/libguides/lookfeel.php?action=0")
+                            p.click("#s-lg-footer_link")
+                            p.fill("#footer_code", html)
+                            p.click("#s-lg-btn-save-footer")
+                            # NOTE must wait for success before moving on
+                            p.wait_for_selector("#s-lg-btn-save-footer.btn-success")
+                        else:
+                            for group in json.loads(groups)["groups"]:
+                                if scope == group["slug"]:
+                                    p.goto(
+                                        f'/libguides/groups.php?action=2&group_id={group["id"]}'
+                                    )
+                                    p.click("#s-lg-footer_link")
+                                    p.fill("#footer_code", html)
+                                    p.click("#s-lg-btn-save-footer")
+                                    # NOTE must wait for success before moving on
+                                    p.wait_for_selector(
+                                        "#s-lg-btn-save-footer.btn-success"
+                                    )
+            b.close()
+        except PlaywrightTimeoutError as e:
+            print(str(e))
+            return
+
+
+if __name__ == "__main__":
+    # fmt: off
+    import plac; plac.call(main)
