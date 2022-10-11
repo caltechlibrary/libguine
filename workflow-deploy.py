51e00c9f420348508475447cf773c8870c52a33e
Tommy Keswick <t4k@users.noreply.github.com>
2022-10-11 16:53:16 -0700

    format quotes for pseudo class

--- workflow-deploy.py
+++ workflow-deploy.py
@@ -85,7 +85,7 @@ def main(
                             p.fill("#widget_name", item.name.split("-", maxsplit=2)[-1].split(".")[0])
                         p.fill("#embed_code", html)
                         p.click("#s-lib-alert-btn-first")
-                        p.wait_for_selector(f'td:text({item.name.split("-", maxsplit=2)[-1].split(".")[0]})')
+                        p.wait_for_selector("td:text('" + item.name.split("-", maxsplit=2)[-1].split(".")[0] + "')")
                     elif target == "head":
                         if scope == "system":
                             p.goto("/libguides/lookfeel.php?action=1")
