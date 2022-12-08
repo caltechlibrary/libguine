db45f53e729922ff7940bf87eb0658364a6b56a7
Tommy Keswick <tkeswick@caltech.edu>
2022-12-08 11:45:29 -0800

    edit comment; trigger deploy

--- libanswers.js
+++ libanswers.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // move the breadcrumbs to the footer
   document.getElementById("footer-breadcrumbs").appendChild(document.querySelector("nav.s-la-breadcrumbs"));
 
-  // this javascript code for the hours widget cannot be added alongside the
-  // HTML, instead it must be entered in a CSS/JS-specific field
+  // include the hours widget code in the JS/CSS field;
+  // scripts cannot be added to the footer field alongside the HTML
   var s_lc_tdh_3271_0 = new $.LibCalTodayHours( $("#s_lc_tdh_3271_0"), { iid: 3271, lid: 0 });
 
   // create a (hard-coded) login link for the footer
