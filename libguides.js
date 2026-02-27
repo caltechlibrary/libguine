54b221dd053d41a8f02616b6cda3beaead22d71f
Twila Smith <7118907+camp5456@users.noreply.github.com>
2026-02-27 07:54:21 -0600

    updated notices template and archives navbar spacing

--- libguides.js
+++ libguides.js
@@ -468,7 +468,8 @@ $(document).ready(function() {
   var checkInterval = setInterval(function() {
     checkCount++;
     const widget = document.getElementById("s-lg-widget-1770820610259");
-    if (widget && widget.getElementsByTagName("ul")[0]) {
+    const ul = widget && widget.getElementsByTagName("ul")[0];
+    if (ul && ul.getElementsByTagName("li").length > 0) {
       clearInterval(checkInterval);
       transformLibraryBlogWidget();
     }
